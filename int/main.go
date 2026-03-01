cb3b485176ac8a02104b5d593a0dfc08f88f7d22
fian <frihkian05@gmail.com>
2026-03-01 13:48:32 +0300

    updated the input func to handle the input and return a slice of the customers input

--- int/main.go
+++ int/main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"bufio"
-	"os"
-	"strconv"
-	"ShoppingCalculator/helper"
-	// "ShoppingCalculator/int/src"
-	// "int/src"
-	// "Shopping-calculator/int/src"
+	"ShoppingCalculator/int/src"
+	
 )
 
 
@@ -22,44 +17,7 @@ import (
   
 	 
 func main() {
-	var input helper.Input
-	// ItemName.item := ""
-	// quantity := ""
-	// price := ""
-	scanner := bufio.NewScanner(os.Stdin)
-	k := []helper.Input{}
-	for {
-		// if scanner.Scan() {
-		fmt.Print("name of the item: ")
-		scanner.Scan()
-		input.ItemName = scanner.Text()
-		if input.ItemName == "" {
-			break
-		}
-
-		fmt.Print("Quantiy of the item: ")
-		scanner.Scan()
-		quantity :=scanner.Text()
-		num, err := strconv.ParseFloat(quantity, 64)
-		if err != nil{
-			fmt.Println("Invalid Quantity")
-		}
-		input.NumberOfItems = quantity
-
-		fmt.Print("Price of the item: ")
-		scanner.Scan()
-		price := scanner.Text()
-		cash, err := strconv.ParseFloat(price, 64)
-			if err != nil {
-			fmt.Println("Invalid Price")
-		}
-		input.PriceOfItem = price
-		val := num * cash
-		input.Cost = strconv.FormatFloat(val, 'f', 2, 64) 
-		// }
-		k = append(k, input)
-		
-	}
-		// int := Input(input.ItemName, input.NumberOfItems, input.PriceOfItem, input.Cost)
-		fmt.Println(k)
+	list := src.Input()
+	
+	fmt.Println(list)
 }
--- int/src/input.go
+++ int/src/input.go
@@ -1,50 +1,55 @@
 package src
 
-// import (
-// 	"ShoppingCalculator/helper"
-// )
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+	// "ShoppingCalculator/helper"
+	"ShoppingCalculator/helper"
+)
 
 
-// func Input(s helper.Input) []helper.Input {
-// 	var input helper.Input
-// 	k := []helper.Input{}
+func Input( ) []helper.Input {
+	var input helper.Input
+	k := []helper.Input{}
 
-// 	scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-// 	for {
-// 		// if scanner.Scan() {
-// 		fmt.Print("name of the item: ")
-// 		scanner.Scan()
-// 		input.ItemName = scanner.Text()
-// 		if input.ItemName == "" {
-// 			break
-// 		}
+	for {
+		// if scanner.Scan() {
+		fmt.Print("name of the item: ")
+		scanner.Scan()
+		input.ItemName = scanner.Text()
+		if input.ItemName == "" {
+			break
+		}
 
-// 		fmt.Print("Quantiy of the item: ")
-// 		scanner.Scan()
-// 		quantity :=scanner.Text()
-// 		num, err := strconv.ParseFloat(quantity, 64)
-// 		if err != nil{
-// 			fmt.Println("Invalid Quantity")
-// 		}
-// 		input.NumberOfItems = quantity
+		fmt.Print("Quantiy of the item: ")
+		scanner.Scan()
+		quantity :=scanner.Text()
+		num, err := strconv.ParseFloat(quantity, 64)
+		if err != nil{
+			fmt.Println("Invalid Quantity")
+		}
+		input.NumberOfItems = quantity
 
-// 		fmt.Print("Price of the item: ")
-// 		scanner.Scan()
-// 		price := scanner.Text()
-// 		cash, err := strconv.ParseFloat(price, 64)
-// 			if err != nil {
-// 			fmt.Println("Invalid Price")
-// 		}
-// 		input.PriceOfItem = price
-// 		val := num * cash
-// 		input.Cost = strconv.FormatFloat(val, 'f', 2, 64) 
-// 		// }
-// 		k = append(k, input)
+		fmt.Print("Price of the item: ")
+		scanner.Scan()
+		price := scanner.Text()
+		cash, err := strconv.ParseFloat(price, 64)
+			if err != nil {
+			fmt.Println("Invalid Price")
+		}
+		input.PriceOfItem = price
+		val := num * cash
+		input.Cost = strconv.FormatFloat(val, 'f', 2, 64) 
+		// }
+		k = append(k, input)
 		
-// 	}
-// 		// int := Input(input.ItemName, input.NumberOfItems, input.PriceOfItem, input.Cost)
-// 		fmt.Println(k)
+	}
+		// int := Input(input.ItemName, input.NumberOfItems, input.PriceOfItem, input.Cost)
+		return k
 
-// }
+}
 

1c1bdea951a87db1f83b8a447084c32188afb371
Frihk <frihkian05@gmail.com>
2026-03-29 16:34:26 +0300

    final update for v 0.01

--- server/main.go
+++ server/main.go
@@ -1,1 +1,25 @@
-package server
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"ShoppingCalculator/api"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", api.HealthHandler)
+	mux.HandleFunc("POST /api/checkout", api.CheckoutHandler)
+	mux.HandleFunc("GET /api/suggestions", api.SuggestionsHandler)
+
+	fileServer := http.FileServer(http.Dir("web"))
+	mux.Handle("/", fileServer)
+
+	addr := ":8080"
+	log.Printf("server listening on http://localhost%s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
